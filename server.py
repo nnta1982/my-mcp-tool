20c7b6d2a8f933bc24a687b41d90a87431f94f5e
TanNguyen <tannguyen@TrangNguyens-MacBook-Air.local>
2026-05-29 21:27:33 +0700

    fix docker

--- server.py
+++ server.py
@@ -11,3 +11,11 @@ def cong_hai_so(a: float, b: float) -> float:
 def nhan_hai_so(a: float, b: float) -> float:
     """Nhân 2 số lại với nhau"""
     return a * b
+
+if __name__ == "__main__":
+    mcp.run(
+        transport="http",
+        host="0.0.0.0",
+        port=8000,
+        allowed_hosts=["*"]
+    )
